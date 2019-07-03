9a413fd257b226ce5c961dd5c7a99f2f4a458c3c
Casey Beaulac <beaulac.casey@gmail.com>
2019-07-03 13:57:08 -0400

    main code done

---
+++ assets/javascript/game.js
@@ -0,0 +1,110 @@
+//game start generate random number
+$(document).ready(function() {
+  // initializing variables
+  var score = 0;
+  var yourNum = 0;
+  var goalNum = 0;
+  var pokeballNum = 0;
+  var greatballNum = 0;
+  var ultraballNum = 0;
+  var masterballNum = 0;
+
+  function restartGame() {
+    yourNum = 0;
+    goalNum = 0;
+    goalNum = Math.floor(Math.random() * 69 + 30);
+    pokeballNum = [Math.floor(Math.random() * 9) + 2];
+    greatballNum = [Math.floor(Math.random() * 9) + 2];
+    ultraballNum = [Math.floor(Math.random() * 9) + 2];
+    masterballNum = [Math.floor(Math.random() * 9) + 2];
+    $("#randomNumDiv").text(goalNum);
+    $("#yourNumDiv").text(yourNum);
+    console.log(pokeballNum);
+    console.log(greatballNum);
+    console.log(ultraballNum);
+    console.log(masterballNum);
+  }
+
+  goalNum = Math.floor(Math.random() * 69 + 30);
+  pokeballNum = [Math.floor(Math.random() * 9) + 2];
+  greatballNum = [Math.floor(Math.random() * 9) + 2];
+  ultraballNum = [Math.floor(Math.random() * 9) + 2];
+  masterballNum = [Math.floor(Math.random() * 9) + 2];
+
+  // setting goal number to div and your number to div
+  $("#randomNumDiv").text(goalNum);
+  $("#yourNumDiv").text(yourNum);
+  $("#winsDiv").text(score);
+  //generate numbers on different pokeballs
+
+  console.log(pokeballNum);
+  console.log(greatballNum);
+  console.log(ultraballNum);
+  console.log(masterballNum);
+
+  $("#pokeball").text(pokeballNum);
+  $("#greatball").text(greatballNum);
+  $("#ultraball").text(ultraballNum);
+  $("#masterball").text(masterballNum);
+
+  $("#pokeball").click(function() {
+    yourNum = parseInt(yourNum) + parseInt(pokeballNum);
+    $("#yourNumDiv").text(yourNum);
+    if (yourNum === goalNum) {
+      confirm("You win! Play again?", true);
+      score++;
+      $("#winsDiv").text(score);
+      restartGame();
+    } else if (yourNum > goalNum) {
+      confirm("You lose! Play again?");
+      restartGame();
+    }
+  });
+
+  $("#greatball").click(function() {
+    yourNum = parseInt(yourNum) + parseInt(greatballNum);
+    $("#yourNumDiv").text(yourNum);
+    if (yourNum === goalNum) {
+      confirm("You win! Play again?", true);
+      score++;
+      $("#winsDiv").text(score);
+      restartGame();
+    } else if (yourNum > goalNum) {
+      confirm("You lose! Play again?");
+      restartGame();
+    }
+  });
+
+  $("#ultraball").click(function() {
+    yourNum = parseInt(yourNum) + parseInt(ultraballNum);
+    $("#yourNumDiv").text(yourNum);
+    if (yourNum === goalNum) {
+      confirm("You win! Play again?", true);
+      score++;
+      $("#winsDiv").text(score);
+      restartGame();
+    } else if (yourNum > goalNum) {
+      confirm("You lose! Play again?");
+      restartGame();
+    }
+  });
+
+  $("#masterball").click(function() {
+    yourNum = parseInt(yourNum) + parseInt(masterballNum);
+    $("#yourNumDiv").text(yourNum);
+    if (yourNum === goalNum) {
+      confirm("You win! Play again?", true);
+      score++;
+      $("#winsDiv").text(score);
+      restartGame();
+    } else if (yourNum > goalNum) {
+      confirm("You lose! Play again?");
+      restartGame();
+    }
+  });
+});
+
+//
+//win? play gain yes/no
+
+//lose? play again yes/no
